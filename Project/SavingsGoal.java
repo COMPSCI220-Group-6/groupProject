a54318b09614b10f24bc1815447a93a1a57d2462
Jacob <115171151+JacobT2006@users.noreply.github.com>
2024-12-03 19:26:10 -0600

    update project for finish

---
+++ Project/SavingsGoal.java
@@ -0,0 +1,13 @@
+package Project;
+
+import javax.swing.*;
+
+public class SavingsGoal extends Transaction{
+    public int savingsGoal(){
+        String message = JOptionPane.showInputDialog(null, "Input Total savings goal", "Savings Goal", JOptionPane.PLAIN_MESSAGE);
+        return Integer.parseInt(message);
+    }
+    public double actualTotal(){
+        return getTotal() + savingsGoal();
+    }
+}
